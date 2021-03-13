0353b1133066fad83ac33d995b967a668599de69
Stefan Fluit <fluit@fedora-desktop.fluit-online.nl>
2021-03-13 23:29:40 +0100

    Changed DNS zone

--- config/config.sh
+++ config/config.sh
@@ -4,5 +4,5 @@ export VPS_ENV="bitwarden"
 export DOMAIN_ENV="example.com"
 export SSH_KEY="/home/$(whoami)/.ssh/id_rsa.pub"
 export SSH_ID_RSA=$(echo "${SSH_KEY}" | cut -f1,2 -d'.')
-export AWS_DNS_ZONE="<Zone ID"
+export AWS_DNS_ZONE="<Zone ID>"
 export ADMIN_MAIL="admin@${DOMAIN_ENV}"
