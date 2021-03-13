8d547363db81fc596ed2bfb1f259d4e011b59861
Stefan Fluit <fluit@fedora-desktop.fluit-online.nl>
2021-03-13 16:57:55 +0100

    Better handling the prereq script

--- config/prereq.sh
+++ config/prereq.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-check_aws() {
+install_aws() {
     wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
     unzip awscli-bundle.zip
     sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
@@ -9,15 +9,15 @@ check_aws() {
     aws configure
 }
 
-check_terraform() {
+install_terraform() {
     curl -LO https://raw.github.com/robertpeteuil/terraform-installer/master/terraform-install.sh
     chmod +x terraform-install.sh
     ./terraform-install.sh
 }
 
 main() {
-    check_aws
-    check_terraform
+    aws &> /dev/null && printf "AWS is installed.\n" || install_aws
+    terraform &> /dev/null && printf "Terraform is installed.\n" || install_terraform
 }
 
 main
