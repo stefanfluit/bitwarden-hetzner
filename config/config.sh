efa626e7dadefd630a12b33c3d1d6927baeb845e
Stefan Fluit <fluit@fedora-desktop.fluit-online.nl>
2021-03-13 23:23:20 +0100

    Set proper config value after tests

--- config/config.sh
+++ config/config.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-export VPS_ENV="bitwarden_test"
-export DOMAIN_ENV="fluit-online.nl"
+export VPS_ENV="bitwarden"
+export DOMAIN_ENV="example.com"
 export SSH_KEY="/home/$(whoami)/.ssh/id_rsa.pub"
 export SSH_ID_RSA=$(echo "${SSH_KEY}" | cut -f1,2 -d'.')
 export AWS_DNS_ZONE="Z1ZA9AAUY4V3Z4"
-export ADMIN_MAIL="stefan@fluit-online.nl"
+export ADMIN_MAIL="admin@${DOMAIN_ENV}"
