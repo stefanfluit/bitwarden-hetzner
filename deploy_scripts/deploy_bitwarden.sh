72cea9e2806360a22742317b2b03a2155b90d14d
Stefan Fluit <fluit@fedora-desktop.fluit-online.nl>
2021-03-13 23:20:38 +0100

    Done for now, should be working

--- deploy_scripts/deploy_bitwarden.sh
+++ deploy_scripts/deploy_bitwarden.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
-set -o errexit      # Exit on uncaught errors
+set -x
 
 declare DIR
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 . "${DIR}/../config/config.sh"
 
-sed -i "s/<ssh-key>/${SSH_KEY_OUTPUT}/g" "${DIR}"/../terraform/user_data.yml
-sed -i "s/<ssh-key>/${SSH_KEY_OUTPUT}/g" "${DIR}"/../terraform/user_data.yml 
-sed -i "s/fqdn/${VPS_ENV}.${DOMAIN_ENV}/g" "${DIR}"/docker-compose/docker-compose.yml
-sed -i "s/email/${ADMIN_MAIL}/g" "${DIR}"/docker-compose/docker-compose.yml 
+if [ ! -e "${SSH_KEY}" ]; then
+    printf "No SSH key found, generatinf one.\n"
+    ssh-keygen -b 4096 -t rsa -f "${SSH_ID_RSA}" -C admin@bitwarden -N ""
+    export SSH_KEY_OUTPUT=$(<${SSH_KEY})
+fi 
+
+sed -i "s|sshkey|${SSH_KEY_OUTPUT}|g" "${DIR}"/../terraform/user_data.yml 
+sed -i "s|fqdn|${VPS_ENV}.${DOMAIN_ENV}|g" "${DIR}"/docker-compose/docker-compose.yml
+sed -i "s|email|${ADMIN_MAIL}|g" "${DIR}"/docker-compose/docker-compose.yml 
 
 printf "Applying Terraform configuration\n"
-cd "${DIR}"/../terraform && terraform init &> /dev/null && terraform plan &> /dev/null && terraform apply -auto-approve &> /dev/null && printf "Done!\n"
+cd "${DIR}"/../terraform && terraform init && terraform plan && terraform apply -auto-approve && printf "Done!\n"
 
 declare BW_IP
 BW_IP=$(terraform output | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
@@ -25,7 +30,7 @@ declare max_timeout="6000"
 declare timeout_at
 timeout_at=$(( SECONDS + max_timeout ))
 
-until ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_ed25519_bitwarden root@"${BW_IP}" '[ -d /var/lib/bitwarden_deploy ]'; do
+until ssh -o StrictHostKeyChecking=no -i "${SSH_ID_RSA}" root@"${BW_IP}" '[ -d /var/lib/bitwarden_deploy ]'; do
   if (( SECONDS > timeout_at )); then
     printf "Maximum time of %s passed, stopping script.\n" "${max_timeout}" >&2
     exit 1
@@ -35,6 +40,7 @@ done
 
 printf "Directory exists, moving forward.\n" && sleep 2
 printf "Copy the docker compose file and caddy file to server..\n"
-scp -i ~/.ssh/id_ed25519_bitwarden -r "${DIR}"/docker-compose root@"${BW_IP}":/tmp &> /dev/null
-ssh -i ~/.ssh/id_ed25519_bitwarden root@"${BW_IP}" "cp -r /tmp/docker-compose/* /var/lib/bitwarden_deploy" &> /dev/null
-ssh -i ~/.ssh/id_ed25519_bitwarden root@"${BW_IP}" "docker-compose -f /var/lib/bitwarden_deploy/docker-compose.yml up -d" &> /dev/null && printf "Done! Access BitWarden now on https://%s.%s\n" "${VPS_ENV}" "${DOMAIN_ENV}"
+scp -i "${SSH_ID_RSA}" -r "${DIR}"/docker-compose root@"${BW_IP}":/tmp &> /dev/null
+ssh -i "${SSH_ID_RSA}" root@"${BW_IP}" "cp -r /tmp/docker-compose/* /var/lib/bitwarden_deploy" &> /dev/null
+ssh -i "${SSH_ID_RSA}" root@"${BW_IP}" "docker-compose -f /var/lib/bitwarden_deploy/docker-compose.yml up -d" &> /dev/null && printf "Done! Access BitWarden now on https://%s.%s\n" "${VPS_ENV}" "${DOMAIN_ENV}"
+printf "Or, connect to the server using SSH: ssh admin@%s.%s.\n" "${VPS_ENV}" "${DOMAIN_ENV}"
