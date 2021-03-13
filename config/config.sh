9f08c08bcb8b2445a32d9262c6fa13830edc05fe
Stefan Fluit <fluit@fedora-desktop.fluit-online.nl>
2021-03-13 23:21:03 +0100

    Set variables to proper value

--- config/config.sh
+++ config/config.sh
@@ -3,6 +3,6 @@
 export VPS_ENV="bitwarden_test"
 export DOMAIN_ENV="fluit-online.nl"
 export SSH_KEY="/home/$(whoami)/.ssh/id_rsa.pub"
-export SSH_KEY_OUTPUT=$(cat ${SSH_KEY})
+export SSH_ID_RSA=$(echo "${SSH_KEY}" | cut -f1,2 -d'.')
 export AWS_DNS_ZONE="Z1ZA9AAUY4V3Z4"
 export ADMIN_MAIL="stefan@fluit-online.nl"
