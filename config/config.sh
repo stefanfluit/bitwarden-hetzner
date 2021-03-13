1d90fe2c46f339d226089ae950a7e0162531fed1
Stefan Fluit <fluit@fedora-desktop.fluit-online.nl>
2021-03-13 22:22:06 +0100

    Fix config

--- config/config.sh
+++ config/config.sh
@@ -2,7 +2,7 @@
 
 export VPS_ENV="bitwarden_test"
 export DOMAIN_ENV="fluit-online.nl"
-export SSH_KEY=$(~/.ssh/id_rsa.pub)
-export SSH_KEY_OUTPUT=$(cat ~/.ssh/id_rsa.pub)
+export SSH_KEY="~/.ssh/id_rsa.pub"
+export SSH_KEY_OUTPUT=$(cat ${SSH_KEY})
 export AWS_DNS_ZONE="Z1ZA9AAUY4V3Z4"
 export ADMIN_MAIL="stefan@fluit-online.nl"
