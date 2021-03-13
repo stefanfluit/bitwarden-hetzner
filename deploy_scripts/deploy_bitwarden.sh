f60a94ef7644a9b675697856428a9011d9bd828b
Stefan Fluit <fluit@fedora-desktop.fluit-online.nl>
2021-03-13 21:51:41 +0100

    Fixed faulty SSH key generation

--- deploy_scripts/deploy_bitwarden.sh
+++ deploy_scripts/deploy_bitwarden.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 ssh-keygen -t ed25519 -f "${SSH_KEY}" -C "${ADMIN_MAIL}" -q -N ""
 declare output_ssh_key
-output_ssh_key=$(< ${SSH_KEY})
+output_ssh_key=$(cat ${SSH_KEY})
 sed -i "s/<ssh-key>/${output_ssh_key}/g" "${DIR}"/../terraform/user_data.yml
 sed -i "s/<ssh-key>/${output_ssh_key}/g" "${DIR}"/../terraform/user_data.yml 
 sed -i "s/fqdn/${VPS_ENV}.${DOMAIN_ENV}/g" "${DIR}"/docker-compose/docker-compose.yml
