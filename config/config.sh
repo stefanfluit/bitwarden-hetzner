0e2245f301fd1be2fcca8d68e5790c93fb4f2e01
Stefan Fluit <fluit@fedora-desktop.fluit-online.nl>
2021-03-14 15:01:21 +0100

    Added some comments to clarify

--- config/config.sh
+++ config/config.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+# Variables you should change:
 export VPS_ENV="bitwarden"
 export DOMAIN_ENV="example.com"
+
+# Variables you can change, but not needed:
 export SSH_KEY="/home/$(whoami)/.ssh/id_rsa.pub"
 export HOST_KEY_FILE="/home/$(whoami)/.ssh/known_hosts"
 export SSH_ID_RSA=$(echo "${SSH_KEY}" | cut -f1,2 -d'.')
-export AWS_DNS_ZONE="<Zone ID>"
 export ADMIN_MAIL="admin@${DOMAIN_ENV}"
 export LOG_LOC="/home/$(whoami)/.BW_HETZNER_RUST.LOG"
 export SOURCE_IP=$(dig @resolver4.opendns.com myip.opendns.com +short)
+
+# Remove this or uncomment if you're not using Route53:
+export AWS_DNS_ZONE="<Zone ID>"
