4cf442ad67e7643692bf3ecd8fbcc70ff4cafb0c
Stefan Fluit <fluit@fedora-desktop.fluit-online.nl>
2021-03-14 12:05:56 +0100

    Added DNS function to easily change method

--- config/functions.sh
+++ config/functions.sh
@@ -8,3 +8,9 @@ cli_log() {
   printf "Bitwarden Hetzner: %s: %s\n" "${timestamp_}" "${arg_}"
   printf "Bitwarden Hetzner: %s: %s\n" "${timestamp_}" "${arg_}" >> "${LOG_LOC}"
 }
+
+set_dns() {
+    local IP_
+    IP_="${1}"
+    aws route53 change-resource-record-sets --hosted-zone-id "${AWS_DNS_ZONE}" --change-batch '{ "Comment": "BitWarden Rust", "Changes": [ { "Action": "CREATE", "ResourceRecordSet": { "Name": "'"${VPS_ENV}"'.'"${DOMAIN_ENV}"'", "Type": "A", "TTL": 120, "ResourceRecords": [ { "Value": "'"${IP_}"'" } ] } } ] }' >> /tmp/aws_log
+}
