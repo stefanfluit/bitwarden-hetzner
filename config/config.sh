717a14026bf768dd7084ea27899556512d04dcd2
Stefan Fluit <fluit@fedora-desktop.fluit-online.nl>
2021-03-14 12:21:21 +0100

    Added host_key_file var

--- config/config.sh
+++ config/config.sh
@@ -3,6 +3,7 @@
 export VPS_ENV="bitwarden"
 export DOMAIN_ENV="example.com"
 export SSH_KEY="/home/$(whoami)/.ssh/id_rsa.pub"
+export HOST_KEY_FILE="/home/$(whoami)/.ssh/known_hosts"
 export SSH_ID_RSA=$(echo "${SSH_KEY}" | cut -f1,2 -d'.')
 export AWS_DNS_ZONE="<Zone ID>"
 export ADMIN_MAIL="admin@${DOMAIN_ENV}"
