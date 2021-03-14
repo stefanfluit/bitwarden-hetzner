124a999db225bb7458b1bc3c7a24021ca66ebacb
Stefan Fluit <fluit@fedora-desktop.fluit-online.nl>
2021-03-14 14:59:06 +0100

    Added WAN IP Variable for restricting SSH

--- config/config.sh
+++ config/config.sh
@@ -8,3 +8,4 @@ export SSH_ID_RSA=$(echo "${SSH_KEY}" | cut -f1,2 -d'.')
 export AWS_DNS_ZONE="<Zone ID>"
 export ADMIN_MAIL="admin@${DOMAIN_ENV}"
 export LOG_LOC="/home/$(whoami)/.BW_HETZNER_RUST.LOG"
+export SOURCE_IP=$(dig @resolver4.opendns.com myip.opendns.com +short)
