29d15bf0cbfe3dfa8424435eba4eb47ad954c257
Stefan Fluit <fluit@fedora-desktop.fluit-online.nl>
2021-03-14 11:58:16 +0100

    Added log location

--- config/config.sh
+++ config/config.sh
@@ -6,3 +6,4 @@ export SSH_KEY="/home/$(whoami)/.ssh/id_rsa.pub"
 export SSH_ID_RSA=$(echo "${SSH_KEY}" | cut -f1,2 -d'.')
 export AWS_DNS_ZONE="<Zone ID>"
 export ADMIN_MAIL="admin@${DOMAIN_ENV}"
+export LOG_LOC="/home/$(whoami)/.BW_HETZNER_RUST.LOG"
